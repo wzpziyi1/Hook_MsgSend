505814177a1e2620c3483b2982fd5d9f7d866e48
wangzhipan <wangzhipan@yy.com>
2019-03-27 17:10:28 +0800

    first commit

---
+++ HookObjc_MsgSend/RecordMethodTime/ZYMethodTraceCore.h
@@ -0,0 +1,17 @@
+//
+//  ZYMethodTraceCore.h
+//  HookObjc_MsgSend
+//
+//  Created by wzp on 2019/3/26.
+//  Copyright © 2019 wzp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZYMethodTraceCore : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HookObjc_MsgSend/RecordMethodTime/ZYMethodTraceCore.mm
@@ -0,0 +1,13 @@
+//
+//  ZYMethodTraceCore.m
+//  HookObjc_MsgSend
+//
+//  Created by wzp on 2019/3/26.
+//  Copyright © 2019 wzp. All rights reserved.
+//
+
+#import "ZYMethodTraceCore.h"
+
+@implementation ZYMethodTraceCore
+
+@end
