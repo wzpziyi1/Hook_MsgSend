d003f860a3c91b83c237769beee6d55e880c3f17
wangzhipan <wangzhipan@yy.com>
2019-04-16 15:21:05 +0800

    更改为mmap存储

--- HookObjc_MsgSend/RecordMethodTime/ZYMethodRecordModel.h
+++ HookObjc_MsgSend/RecordMethodTime/ZYMethodRecordModel.h
@@ -8,15 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
-
 @interface ZYMethodRecordModel : NSObject
 @property (nonatomic, copy) NSString *className;
 @property (nonatomic, copy) NSString *methodName;
 @property (nonatomic, assign) BOOL isClassMethod;       //是否类方法
 @property (nonatomic, assign) NSUInteger callDepth;     //层级
 @property (nonatomic, assign) NSUInteger timeCost;      //耗时
-
-@property (nonatomic, strong) NSMutableArray<ZYMethodRecordModel *> *subRecordModelArr;
 
 - (NSString *)descInfo;
 @end
